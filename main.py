894e7c0c4e1399fea5de304d9e9e4f469586a413
nataloska <natalia.drnkova@gmail.com>
2020-10-25 12:54:21 +0000

    index & functionality recipeform

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import pymongo
 import os
 import gridfs
+import logging
+import traceback
+import base64
 
+from pprint import pprint
 from flask import Flask, render_template, request, redirect, url_for, session, send_from_directory
 from pymongo import TEXT
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import StringField, PasswordField, TextAreaField
 from wtforms.validators import Email, Length, InputRequired, DataRequired
 from passlib.hash import sha256_crypt
 from datetime import datetime
-from werkzeug.datastructures import CombinedMultiDict, MultiDict
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import CombinedMultiDict, MultiDict, FileStorage
 
 app = Flask(__name__)
 
@@ -23,9 +28,8 @@ RECIPE_COLLECTION = "recipe"
 db = pymongo.MongoClient(MONGODB_URI)
 users = db[DBS_NAME][USER_COLLECTION]
 recipes = db[DBS_NAME][RECIPE_COLLECTION]
-recphoto = db[DBS_NAME]
-fs = gridfs.GridFS(recphoto)
 recipesearch = recipes.create_index([('description',TEXT)],default_language="english")
+fs = gridfs.GridFS(db[DBS_NAME])
 
 class LoginForm(FlaskForm):
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=30)])
@@ -40,20 +44,29 @@ class RegForm(FlaskForm):
 class SearchForm(FlaskForm):
     search = StringField('search', validators=[InputRequired(), Length(max=30)])
 
+
 class RecipeForm(FlaskForm):
     recipename = StringField('recipename', validators=[InputRequired(), Length(max=30)])
-    photo = FileField('photo')
+    photo = FileField('photo', validators=[FileRequired()])
     ingredients = StringField('ingredients')
     description = StringField('description')
     instructions = StringField('instructions')
     cTime = StringField('cTime')
     pTime = StringField('pTime')
 
-
 # Flask
 @app.route("/")
 def index():
-    return render_template("index.html")
+    recentrecipes = []
+    recententries = db[DBS_NAME][RECIPE_COLLECTION].find().sort('date' , pymongo.DESCENDING).limit(5)
+    for entry in recententries:
+        customername = users.find_one({'email' : entry['username']})['name']
+        entry['customername'] = customername
+        recentrecipes.append(entry)
+        newdate = datetime.date(entry['date'])
+        entry['date'] = newdate
+        break
+    return render_template("index.html", recentrecipes=recentrecipes)
 
 @app.route("/search", methods=["POST"])
 def search():
@@ -67,15 +80,20 @@ def search():
 
 @app.route("/recipe")
 def recipe():
-    homeresults = recipes.find().limit(9).sort('date',pymongo.DESCENDING)
+    homeresults = recipes.find({}).sort('date',pymongo.DESCENDING).limit(9)
     return render_template("recipe.html", homeresults=homeresults)
 
-@app.route("/newrecipe", methods=['POST'])
+@app.route("/newrecipe", methods=['POST', 'GET'])
 def newrecipe(): 
-    form = RecipeForm(CombinedMultiDict([request.files, request.form]))
-    print('validatefail')
-    if form.validate():
-        print('validatesuccess')
+    form = RecipeForm(CombinedMultiDict([request.files, request.form]))  
+    if form.validate_on_submit():
+        files_dir = os.path.join(
+            os.path.dirname(app.instance_path), 'static'
+        )
+        p = form.photo.data
+        filename = secure_filename(p.filename)
+        print(filename)
+        p.save(os.path.join(files_dir, 'recipe', filename))
         currentuser = users.find_one({'email' : session['email']})
         if currentuser:
             recipes.insert_one({'username' : session['email'], 'recipename' : form.recipename.data,
@@ -83,13 +101,19 @@ def newrecipe():
                                 'instructions' : form.instructions.data, 'cooktime' : form.cTime.data,
                                 'preptime' : form.pTime.data, 'date' : datetime.now()})
             recipe_id = recipes.find_one({'username' : session['email'], 'recipename' : form.recipename.data})['_id']
-            try: 
-                with open(form.phone.data, 'rb') as blob:
-                    fs.put(blob, recipe_id = recipe_id)
-                    return redirect(url_for('index'))
-            except EnvironmentError:
-                print('No photo to upload')
-            
+            print(recipe_id)
+            try:
+                with open('static/recipe/' + filename, "rb") as image:
+                    image_string = base64.b64encode(image.read())
+                    fs.put(image_string, filename = filename, recipe_id = recipe_id)
+                if os.path.isfile('static/recipe/' + filename):
+                    os.remove('static/recipe/' + filename)
+                else: 
+                    print("Error: %s file not found" % myfile)
+                return  redirect(url_for('index'))
+            except Exception as e:
+                logging.error(traceback.format_exc())
+                return render_template("newrecipe.html", form=form)       
     return render_template("newrecipe.html", form=form)
 
 @app.route("/settings")
@@ -132,7 +156,7 @@ def logout():
 
 if __name__ == "__main__":
     app.secret_key = '12346894741389410'
-    app.config['MAX_CONTENT_LENGTH'] = 2* 1024 * 1024    
+    app.config['MAX_CONTENT_LENGTH'] = 2* 1024 * 1024 
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
